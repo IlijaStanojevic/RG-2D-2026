31c0d0eafd3380e2503ec0ae36dc5175124ef7cf
Ilija Stanojevic <ilija.stanojevic123@gmail.com>
2025-12-06 04:13:26 +0100

    Late init project, already has map texture and compass configured

---
+++ Kostur/Source/Main.cpp
@@ -0,0 +1,192 @@
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+#define _USE_MATH_DEFINES
+#include <cmath> // za pi
+#include <algorithm> // za max()
+#include <iostream>
+#include "../Header/Util.h"
+
+
+
+
+
+
+
+int screenWidth = 1080;
+int screenHeight = 1920;
+
+float uX = 0.0f; 
+float uY = 0.0f; 
+float uS = 1.0f;
+
+unsigned mapTexture;
+
+GLFWcursor* cursor;
+GLFWcursor* cursorPressed;
+
+
+void preprocessTexture(unsigned& texture, const char* filepath) {
+    texture = loadImageToTexture(filepath); // Učitavanje teksture
+    glBindTexture(GL_TEXTURE_2D, texture); // Vezujemo se za teksturu kako bismo je podesili
+
+    // Generisanje mipmapa - predefinisani različiti formati za lakše skaliranje po potrebi (npr. da postoji 32 x 32 verzija slike, ali i 16 x 16, 256 x 256...)
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // Podešavanje strategija za wrap-ovanje - šta da radi kada se dimenzije teksture i poligona ne poklapaju
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // S - tekseli po x-osi
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // T - tekseli po y-osi
+
+    // Podešavanje algoritma za smanjivanje i povećavanje rezolucije: nearest - bira najbliži piksel, linear - usrednjava okolne piksele
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void formVAOs() {
+
+
+
+}
+
+// Projekat je dozvoljeno pisati počevši od ovog kostura
+// Toplo se preporučuje razdvajanje koda po fajlovima (i eventualno potfolderima) !!!
+// Srećan rad!
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // Formiranje prozora za prikaz sa datim dimenzijama i naslovom
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    screenWidth = mode->width;
+    screenHeight = mode->height;
+    // Uzimaju se širina i visina monitora
+    // Moguće je igrati se sa aspect ratio-m dodavanjem frame buffer size callback-a
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "Vezba 3", monitor, NULL);
+    if (window == NULL) return endProgram("Prozor nije uspeo da se kreira.");
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) return endProgram("GLEW nije uspeo da se inicijalizuje.");
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    preprocessTexture(mapTexture, "resources/novi-sad-map-0.jpg");
+    cursor = loadImageToCursor("resources/bolji-compass.png");
+    glfwSetCursor(window, cursor);
+
+    unsigned int rectShader = createShader("rect.vert", "rect.frag");
+    glUseProgram(rectShader);
+    glUniform1i(glGetUniformLocation(rectShader, "uTex0"), 0);
+    glUniform1i(glGetUniformLocation(rectShader, "uTex1"), 1);
+
+
+    unsigned int basicShader = createShader("basic.vert", "basic.frag");
+
+    float vertices[] = {
+         -0.2f, 0.2f, 0.0f, 0.0f, 1.0f, // gornje levo teme
+         -0.2f, -0.2f, 0.0f, 1.0f, 0.0f, // donje levo teme
+         0.2f, -0.2f, 1.0f, 0.0f, 0.0f, // donje desno teme
+    };
+    unsigned int VAOnight;
+    size_t nightSize = sizeof(vertices);
+    unsigned int VBOnight;
+    glGenVertexArrays(1, &VAOnight);
+    glGenBuffers(1, &VBOnight);
+
+    glBindVertexArray(VAOnight);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOnight);
+    glBufferData(GL_ARRAY_BUFFER, nightSize, vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Atribut 1 (boja):
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+
+    float verticesRect[] = {
+        // x, y,  u, v
+        -1.0f,  1.0f,   0.0f, 1.0f,   // top left
+        -1.0f, -1.0f,   0.0f, 0.0f,   // bottom left
+         1.0f, -1.0f,   1.0f, 0.0f,   // bottom right
+         1.0f,  1.0f,   1.0f, 1.0f    // top right
+    };
+
+
+
+    unsigned int VAOrect;
+    size_t rectSize = sizeof(verticesRect);
+    unsigned int VBOrect;
+    glGenVertexArrays(1, &VAOrect);
+    glGenBuffers(1, &VBOrect);
+
+    glBindVertexArray(VAOrect);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOrect);
+    glBufferData(GL_ARRAY_BUFFER, rectSize, verticesRect, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+
+    glClearColor(0.2f, 0.8f, 0.6f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        double initFrameTime = glfwGetTime(); 
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) { // esc = close
+            break;
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+        glUseProgram(basicShader); // Podešavanje da se crta koristeći dati šejder
+        glBindVertexArray(VAOnight); // Podešavanje da se crta koristeći date vertekse
+
+        // Crtaju se trouglovi, krećući se od nultog elementa u vertices i zaustavljajući se posle 3 temena
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glBindVertexArray(VAOrect);
+        //lDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        // 
+        // 
+        glUseProgram(rectShader);
+
+        glUniform1f(glGetUniformLocation(rectShader, "uX"), uX); // Zadatak 3
+        glUniform1f(glGetUniformLocation(rectShader, "uY"), uY); // Zadatak 2
+        glUniform1f(glGetUniformLocation(rectShader, "uS"), uS); // Zadatak 4
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mapTexture);
+
+
+        glBindVertexArray(VAOrect);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+        
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+
+
+
+        while (glfwGetTime() - initFrameTime < 1 / 75.0) {} // fps limiter
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
